d10b4441c92771e1291364cd936da875c0532826
Jatin <jatinjayadev@gmail.com>
2024-09-23 13:35:10 +0530

    Database read and write Performed

--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,32 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const mongoose = require('mongoose')
+
+const authRoute = require('./routes/auth')
+const restaurantRoute = require('./routes/Restaurant')
 
 app.use(cors())
 app.use(express.json())
 
+app.use("/", authRoute)
+app.use("/", restaurantRoute)
+
 require('dotenv').config()
 
 const port = process.env.PORT || 4050;
+const URI = process.env.URI
 
-app.get('/', (req, res) => {
-    res.send("Working!!!!")
-});
+mongoose.connect(URI)
+    .then(() => {
+        console.log("Server is connected to database!")
+        app.get('/', (req, res) => {
+            res.send("Working!!!!")
+        });
+    })
+    .catch((err) => {
+        console.log("Database Error", err)
+    })
 
 app.listen(port, () => {
     console.log(`This is from port ${port}`)
