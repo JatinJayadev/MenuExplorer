f7b42328db214f66c038c3163f908ba5d66977fc
Jatin <jatinjayadev@gmail.com>
2024-09-24 14:50:18 +0530

    Made some changes

--- Server/server.js
+++ Server/server.js
@@ -3,9 +3,6 @@ const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 
-const cron = require('node-cron');
-const axios = require('axios');
-
 const authRoute = require('./routes/auth')
 const restaurantRoute = require('./routes/Restaurant')
 
@@ -20,30 +17,12 @@ require('dotenv').config()
 const port = process.env.PORT || 4050;
 const URI = process.env.URI
 
-function setupCronJob() {
-    const renderAPIUrl = process.env.RENDERLINK;
-
-    cron.schedule('*/5 * * * *', async () => {
-        const currentTime = new Date().toLocaleString();
-        try {
-            const response = await axios.get(renderAPIUrl);
-            console.log(`Pinged Render API at ${currentTime}: Status ${response.status}`);
-        } catch (error) {
-            console.error(`Failed to ping Render API at ${currentTime}: ${error.message}`);
-        }
-    });
-
-    console.log('Cron jobs is ready!');
-}
-
 mongoose.connect(URI)
     .then(() => {
         console.log("Server is connected to database!")
         app.get('/', (req, res) => {
             res.send("Working!!!!")
         });
-
-        setupCronJob();
     })
     .catch((err) => {
         console.log("Database Error", err)
